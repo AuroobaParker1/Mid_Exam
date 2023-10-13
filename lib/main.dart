b196978418c33af6f922c8f25624c06e2a04800d
aurooba12 <143499668+aurooba12@users.noreply.github.com>
2023-10-13 10:51:57 +0500

    Exam Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Products {
+  int? id;
+  String title;
+  String? description;
+  int? price;
+  double? discountPercentage;
+  dynamic rating;
+  int? stock;
+  String? brand;
+  String? category;
+  String? thumbnail;
+  List<dynamic> images;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  Products({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.discountPercentage,
+    required this.rating,
+    required this.stock,
+    required this.brand,
+    required this.category,
+    required this.thumbnail,
+    required this.images,
+  });
+
+  factory Products.fromJson(Map<String, dynamic> json) {
+    return Products(
+      description: json['description'],
+      id: json['id'],
+      title: json['title'],
+      brand: json['brand'],
+      price: json['price'],
+      discountPercentage: json['discountPercentage'],
+      rating: json['rating'],
+      stock: json['stock'],
+      category: json['category'],
+      thumbnail: json['thumbnail'],
+      images: json['images'],
     );
   }
 }
 
+Future<List<Products>> fetchProducts() async {
+  final response = await http.get(Uri.parse('https://dummyjson.com/products'));
+
+  if (response.statusCode == 200) {
+    var data = json.decode(response.body)['products'] as List;
+    List<Products> productsList =
+        data.map((i) => Products.fromJson(i)).toList();
+    return productsList;
+  } else {
+    throw Exception('Failed to load products');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Products',
+      home: const MyHomePage(title: 'Products'),
+    );
+  }
+}
+
+void _bottomSheet(BuildContext context, Products data) {
+  showModalBottomSheet(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      ),
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext bc) {
+        return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: data.images.map((i) => Image.network(i)).toList(),
+                  ),
+                ),
+                Text(data.title),
+                Text(data.description!),
+                Text("${data.price}"),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.star),
+                          Text(data.rating.toString())
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(data.discountPercentage.toString()),
+                          Icon(Icons.discount),
+                        ],
+                      )
+                    ])
+              ],
+            ));
+      });
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -59,67 +132,66 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(style: TextStyle(color: Colors.black), widget.title),
+          centerTitle: true,
+          elevation: 0,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: FutureBuilder<List<Products>>(
+          future: fetchProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<Products>? data = snapshot.data;
+              return ListView.builder(
+                  itemCount: data!.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                      color: Colors.lightGreen.shade50,
+                      child: Column(children: [
+                        Image.network(data[index].thumbnail!),
+                        Card(
+                            clipBehavior: Clip.antiAlias,
+                            color: Colors.lightGreen.shade50,
+                            child: Column(children: [
+                              Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(data[index].title),
+                                    Row(children: [
+                                      Text(" ${data[index].price} USD "),
+                                      GestureDetector(
+                                          onTap: () {
+                                            _bottomSheet(context, data[index]);
+                                          },
+                                          child:
+                                              Icon(Icons.remove_red_eye_sharp))
+                                    ])
+                                  ]),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [Text(data[index].description!)],
+                              )
+                            ]))
+                      ]),
+                    );
+                  });
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        ));
   }
 }
